47e283bcb6c10c0c5944a616a2f21fe712c8040e
ohymns <olivia.heymanns@code.berlin>
2024-03-12 15:50:00 +0100

    modified server

--- server.js
+++ server.js
@@ -1,20 +1,37 @@
 import express from 'express';
 import path from 'path';
-const app = express()
-const PORT = 3000
-app.use(express.static('public'));
 
-// Route for the CV page
-app.get('/cv', function(req, res) {
-    res.sendFile(path.join(__dirname, '/public/download-cv/cv.html'));
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the public directory
+app.use(express.static(path.join(process.cwd(), 'public')));
+
+// Route for the landing page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(process.cwd(), '/public/landing.html'));
 });
 
-// Default route
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '/public/landing.html'));
+// Route for the CV download page
+app.get('/cv', (req, res) => {
+    res.sendFile(path.join(process.cwd(), '/public/download-cv/cv.html'));
 });
 
+// Route for the hire me page
+app.get('/hire-me', (req, res) => {
+    res.sendFile(path.join(process.cwd(), '/public/hire-now/hire-now.html'));
+});
+
+// Route for the portfolio
+app.get('/portfolio', (req, res) => {
+    res.sendFile(path.join(process.cwd(), '/public/portfolio-page/portfolio.html'));
+});
+
+// Route for the portfolio
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(process.cwd(), '/public/about-me/about.html'));
+});
 // Start the server
-app.listen(PORT, function() {
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
