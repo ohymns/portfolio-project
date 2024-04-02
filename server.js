21456983a3ff9bcba752268c896c06338aed785b
ohymns <olivia.heymanns@code.berlin>
2024-04-02 14:34:50 +0200

    connecting to database, establishing post, a ejs file, creating a scema and linking it uo to ejs file, update file, delete file. make blogposts

--- server.js
+++ server.js
@@ -4,17 +4,42 @@ import path from 'path';
 import { CLIENT_RENEG_LIMIT } from 'tls';
 import mongoose from 'mongoose'
 import { error } from 'console';
+import { stringify } from 'querystring';
+import { url } from 'inspector';
+//import { connectToDb , getDb } from './db';
 
 const app = express();
 app.set('view engine' , 'ejs')
-const PORT = process.env.PORT || 3000;
-mongoose.connect('mongodb+srv://oliviaheymanns:IndecisiveAF@database-test.2aeusoi.mongodb.net/')
-    .then(()=> console.log("∞∞Database∞∞Connected"))
-    .catch(error => console.error(error))
-
-
+app.use(express.json());
 // Serve static files from the public directory
 app.use(express.static(path.join(process.cwd(), 'public')));
+
+const PORT = process.env.PORT || 3000;
+//connecting to mongoDB database
+mongoose.connect('mongodb+srv://oliviaheymanns:EasyPeasy@database-test.2aeusoi.mongodb.net/?retryWrites=true&w=majority&appName=database-test')
+    .then(()=> console.log("Database Connected"))
+    .catch(error => console.error(error))
+//set database
+const portfolioSchema = mongoose.Schema({
+    author: { type: String, required: true },
+    date: { type: Date, required: true } ,
+    img_url: { type: String, required: true },
+    title: { type: String, required: true } , 
+    discription: { type: String, required: true } , 
+    tags: { type: String, required: true }
+})
+const Image = mongoose.model('Image', portfolioSchema)
+
+const blogSchema = new mongoose.Schema({
+    author: { type: String, required: true },
+    date: { type: String, required: true } ,
+    img_url: { type: String, required: true },
+    title: { type: String, required: true } , 
+    content: { type: String, required: true } , 
+    tags: { type: String, required: true }
+})
+const Blogpost = mongoose.model('Blogpost' , blogSchema)
+
 
 // Route for the landing page
 //app.get('/', (req, res) => {
@@ -43,6 +68,21 @@ app.get('/portfolio', (req, res) => {
 app.get('/about', (req, res) => {
     res.sendFile(path.join(process.cwd(), '/public/about-me/about.html'));
 });
+
+// Route for the blog
+app.get('/blog', async (req, res) => {
+    res.render('blog', {blogposts: await Blogpost.find({}).exec()});
+});
+//update post 
+app.patch('/blog/:id' , async (req , res) => {
+    let updateData = req.body
+    let newPost = await Blogpost.findByIdAndUpdate(req.params.id , updateData).exec()
+    res.json(newPost)
+})
+
+app.delete('/blog/:id' , async (req , res) => {
+     await Blogpost.deleteOne({_id : req.params.id})
+})
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
@@ -81,5 +121,24 @@ app.get('/api/v1/services' , (request,response) =>{
      })
     })
 
+app.post('/blog', async (req , res ) => {
+    try {
+        const blogpost = new Blogpost({
+            author: "Olivia Heymanns",
+            date: "01.04.24",
+            img_url: "http//",
+            title: "Event shoot for ***", 
+            content: "This weekend, I was capturing *** for ***" , 
+            tags: "events"
+        })
+        await blogpost.save()
 
-    
+        res.send('Blog post made :)')
+    }catch (error) {
+        console.error(error)
+        res.send('Error: The cookie could not be created.') 
+    } 
+})
+
+
+
